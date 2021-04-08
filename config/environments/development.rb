4e2fc0d3f71022df81ee5ba3edd2f6f339972286
Deyna Contreras <deyna.alexac@gmail.com>
2021-04-08 16:05:58 -0400

    push to heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.assets.initialize_on_precompile = false
   # Settings specified here will take precedence over those in config/application.rb.
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # In the development environment your application's code is reloaded on
