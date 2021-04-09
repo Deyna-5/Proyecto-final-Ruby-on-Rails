85527aeea9a876b55c95865f269b60a81df057a9
Deyna Contreras <deyna.alexac@gmail.com>
2021-04-09 15:09:19 -0400

    Creating cart

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,40 @@
 class Order < ApplicationRecord
-  
+  before_create :generate_number
+
   belongs_to :user
   has_many :order_items
   has_many :products, through: :order_items
   has_many :payments
+
+  def generate_number(size = 9)
+    self.number ||= loop do
+      random = random_candidate(size)
+      break random unless self.class.exists?(number: random)
+    end
+  end
+
+  def random_candidate(size = 9)
+    "#{has_prefix}#{Array.new(size){rand(size)}.join}"
+  end
+
+  def has_prefix
+    "BO"
+  end
+
+  def add_product(product_id, quantity)
+    product = Product.find(product_id)
+    if product
+      order_items.create(product_id: product.id, quantity: quantity, price: product.price)
+      compute_total
+    end
+  end
+
+  def compute_total
+    sum = 0
+    order_items.each do |item|
+      sum += item.price
+    end
+    update_attribute(:total, sum)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     sessions: "users/sessions"
   }
 
+  resource :cart, only: [:show, :update]
   resources :products, only: [:show, :new, :create, :edit, :update, :destroy]
 
   get 'update/:id', to: 'users#become_admin', as: 'become_admin'
@@ -12,7 +13,5 @@ Rails.application.routes.draw do
   
   get "dashboard", to: "dashboard#index", as: "dashboard"
 
-  #Root Path solo para pruebas momentaneas
   root "home#index"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
