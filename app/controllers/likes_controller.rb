05541fe56d18954a27fc4d4059d4d98b3d509fc2
Deyna Contreras <deyna.alexac@gmail.com>
2021-04-09 21:49:36 -0400

    Like and dislike

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,14 +1,14 @@
 class LikesController < ApplicationController
        before_action :find_product
-       before_action :find_like, only: [:destroy]
+       before_action :find_like, only: :destroy
 
        def create
               if already_liked?
                      flash[:notice] = "Solo le puedes dar like una vez a este producto"
               else
                      @product.likes.create(user_id: current_user.id)
+                     redirect_to root_path, notice:"Haz dado like a un producto!"
               end
-              redirect_to root_path
        end
 
        def destroy
@@ -17,20 +17,20 @@ class LikesController < ApplicationController
               else 
                      @like.destroy
               end
-              redirect_to root_path
+              redirect_to root_path, notice: "Le haz dado unlike a un producto"
        end
 
        private
-       def already_liked?
-              Like.where(user_id: current_user.id, product_id: params[:product_id].exists?)
+       def find_product
+              @product = Product.find(params[:product_id])
        end
 
        def find_like
               @like = @product.likes.find(params[:id])
        end
 
-       def find_product
-              @product = Product.find(params[:product_id])
+       def already_liked?
+              Like.where(user_id: current_user.id, product_id: params[:product_id]).exists?
        end
 
 end
