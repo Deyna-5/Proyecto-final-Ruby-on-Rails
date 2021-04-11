55280915aefe43002f974d0364fade3e3d022b31
Deyna Contreras <deyna.alexac@gmail.com>
2021-04-11 18:04:00 -0400

    adding dependents

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :orders
-  has_many :comments
+  has_many :orders, dependent: :destroy
+  has_many :comments, dependent: :destroy
   has_one_attached :avatar
-  has_many :likes
+  has_many :likes, dependent: :destroy
 end
