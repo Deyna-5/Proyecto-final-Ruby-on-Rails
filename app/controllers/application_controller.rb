f57c0c3cee3bd51ce78398098961d2010d8a9715
Deyna Contreras <deyna.alexac@gmail.com>
2021-04-12 17:15:06 -0400

    fixing error pay with paypal

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
 
        def current_order
-              if current_order
+              if current_user
                      order = Order.where(user_id: current_user.id).where(state: "created").last
                      if order.nil?
                             order = Order.create(user: current_user, state: "created")
